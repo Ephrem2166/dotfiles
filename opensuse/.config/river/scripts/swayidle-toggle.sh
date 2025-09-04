4cd9cabb3926282fa4a10f349ebe6aff231e3f29
Ephrem2166 <ephrem2166@gmail.com>
2025-09-04 13:39:21 +0300

    riverwm config

---
+++ opensuse/.config/river/scripts/swayidle-toggle.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+if pgrep swayidle >/dev/null; then
+  pkill swayidle
+  notify-send -a center_notify "Swayidle" "Disabled" -t 1500 -h string:x-canonical-private-synchronous:volume
+else
+  notify-send -a center_notify "Swayidle" "Enabled" -t 1500 -h string:x-canonical-private-synchronous:volume
+  swayidle -w \
+    timeout 1600 "swaylock -f" \
+    timeout 1800 "wlopm --off '*'" resume "wlopm --on '*'" \
+    before-sleep "swaylock -f" &
+fi
