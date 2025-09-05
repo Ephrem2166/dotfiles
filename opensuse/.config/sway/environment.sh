e53c2d4bac2a18249e55362d0f8bbf85fb5dfff2
Ephrem2166 <ephrem2166@gmail.com>
2025-09-05 13:47:00 +0300

    sway config

---
+++ opensuse/.config/sway/environment.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+export MOZ_ENABLE_WAYLAND=1
+export QT_QPA_PLATFORM="wayland"
+
+export XCURSOR_SIZE=24
+export XCURSOR_THEME="Adwaita"
+export XDG_CURRENT_DESKTOP="sway"
+export XDG_SESSION_TYPE="wayland"
+export XDG_SESSION_DESKTOP="sway"
+export QT_QPA_PLATFORMTHEME="qt6ct"
+export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
+export QT_AUTO_SCREEN_SCALE_FACTOR=1
+export SDL_VIDEODRIVER="wayland"
+export GTK_USE_PORTAL=0
+export _JAVA_AWT_WM_NONREPARENTING=1
