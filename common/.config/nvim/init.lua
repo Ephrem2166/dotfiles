4a5f94ca57c7c4cda2a49df353da13653c2d0b44
Ephrem2166 <ephrem2166@gmail.com>
2025-09-16 14:52:06 +0300

    nvim config

--- common/.config/nvim/init.lua
+++ common/.config/nvim/init.lua
@@ -1,2 +1,18 @@
-require("config.settings")
-require("config.mappings")
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.g.have_nerd_font = false
+
+vim.o.number = true
+
+vim.o.mouse = 'a'
+
+vim.o.showmode = false
+
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.schedule(function()
+  vim.o.clipboard = 'unnamedplus'
+end)
