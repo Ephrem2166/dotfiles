c2b23d873070524d480ec4463fe01bdaaec94cb2
Ephrem2166 <ephrem2166@gmail.com>
2025-09-17 11:28:51 +0300

    lazy nvim setup

--- common/.config/nvim/lua/configs/package.lua
+++ common/.config/nvim/lua/configs/package.lua
@@ -12,4 +12,44 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
+require("lazy").setup({
+  spec = {
+    {
+      "LazyVim/LazyVim",
+      import = "lazyvim.plugins",
+      opts = {
+        colorscheme = "catppuccin",
+      },
+    },
+    {
+      import = "plugins",
+    },
+  },
+  ui = {
+    backdrop = 100,
+  },
+  defaults = {
+    lazy = true,
+    version = false, -- always use the latest git commit
+    -- version = "*", -- try installing the latest stable version for plugins that support semver
+  },
+  local_spec = true,
+  checker = { enabled = true }, -- automatically check for plugin updates
+  performance = {
+    cache = {
+      enabled = true,
+      -- disable_events = {},
+    },
+    rtp = {
+      -- disable some rtp plugins
+      disabled_plugins = {
+        "gzip",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "netrwPlugin",
+        "zipPlugin",
+      },
+    },
+  },
+})
