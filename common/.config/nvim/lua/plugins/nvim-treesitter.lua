592d964be54fa026ad69774e781c0ea66ecd8a2b
Ephrem2166 <ephrem2166@gmail.com>
2025-12-04 12:27:25 +0300

    updated nvim

--- common/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ common/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,4 +1,49 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
+    opts ={
+	indent = {
+	enable = true
+	},
+	highlight = {
+	enable = true,
+	additional_vim_regex_highlighting = { "markdown" },
+	},
+	folds = {
+	enable = true
+	},
+	ensure_installed = {
+    "bash",
+      "c",
+      "css",
+      "cpp",
+      "diff",
+      "go",
+      "html",
+      "java",
+      "javascript",
+      "jsdoc",
+      "json",
+      "jsonc",
+      "lua",
+      "luadoc",
+      "luap",
+      "markdown",
+      "markdown_inline",
+      "printf",
+      "python",
+      "query",
+      "regex",
+      "rust",
+      "toml",
+      "tsx",
+      "typescript",
+      "vim",
+      "vimdoc",
+      "xml",
+      "yaml",
+	},
+    },
+    sync_install = false,
+    auto_install = true,
 }
