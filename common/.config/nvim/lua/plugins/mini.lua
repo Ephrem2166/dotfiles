72cba37b6e58405733bde0bed0992af1fb22ec5b
Ephrem2166 <ephrem2166@gmail.com>
2025-12-05 15:30:51 +0300

    updated nvim

---
+++ common/.config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,24 @@
+return {
+	"nvim-mini/mini.nvim",
+	version = false,
+	config = function()
+		require("mini.ai").setup()
+		require("mini.align").setup()
+		require("mini.comment").setup()
+		require("mini.completion").setup()
+		require("mini.extra").setup()
+		require("mini.files").setup()
+		require("mini.hipatterns").setup()
+		require("mini.indentscope").setup()
+		require("mini.pairs").setup()
+		require("mini.pick").setup()
+		require("mini.surround").setup()
+		require("mini.trailspace").setup()
+		require("mini.move").setup({
+			mappings = {
+				line_up = "<a-UP>",
+				line_down = "<a-DOWN>",
+			},
+		})
+	end,
+}
